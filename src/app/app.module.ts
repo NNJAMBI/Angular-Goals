c486fae092a0f4720960efa6d41d64523dd54fc0
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-09-29 11:19:19 +0300

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent
   ],
   imports: [
     BrowserModule
